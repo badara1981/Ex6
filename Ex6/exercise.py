36e2b9b793e16d5cad2481aaf4054cc228273246
badara1981 <badara.jammeh@hotmail.com>
2022-08-21 20:19:21 +0200

    added

--- Ex6/exercise.py
+++ Ex6/exercise.py
@@ -1,8 +1,9 @@
 for n in range(1,101):
-    
-    if( n%3 == 0):
+    if( n % 3 == 0 and  n % 5 == 0):
+        print("Fizz Buzz")
+if( n % 3 == 0):
         print("Fizz")
-    elif(n% 5==0):  
+elif( n % 5 == 0 ):  
         print("Buzz")
 else:
     print(n)
