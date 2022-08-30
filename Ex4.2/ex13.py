5cfd60427256c6aaa51c876daaa59834acea326b
badara1981 <badara.jammeh@hotmail.com>
2022-08-30 15:16:50 +0200

    added

---
+++ Ex4.2/ex13.py
@@ -0,0 +1,32 @@
+
+from posixpath import split
+from random import shuffle 
+list  = [ 10, 20, 30, 40, 100]
+print(min(list))
+
+print(max(list))
+
+myList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+shuffle(myList)
+
+#
+print("Today is  my Birthday party")
+print('Am going to the beach')
+print("we are going to Rio Jeneiro 'We are in Brazil'")
+print('We are now on my trip to Berlin')
+print("hello" +  " world")
+greeting = "Hello"
+name = input("write name of someone here: ")
+print(greeting+  ' ' +name)
+name = input("Please enter your name")
+print(greeting + ' ' + name) 
+
+#  The Escape  Character  in String  using 
+# Backlash
+# Here we using SPLIT OPERATOR to break the string into various 
+
+splitString = "This string has been! \nsplit over \nseveral  \nlines"
+print(splitString)
+
+tabbedString = "1\t2, \t3, \t4,\t5"
+print(tabbedString)
