018df9aea87441207d0dea96598f63f264e8930a
badara.jammeh@hotmail.com <badara.jammeh@hotmail.com>
2022-09-03 16:44:39 +0200

    added

--- Ex4.2/ex_14.py
+++ Ex4.2/ex_14.py
@@ -7,3 +7,12 @@ type(result)
 print(float(result))
 
 print(int(result))
+
+#List comprehension is a unique quickly  way of creating a list in with python.
+# if you find your self using for loops along with .append() to create a list.
+# list comprehension are good alternatives
+# To do this let go to Jupiter NoteBook
+myList = []
+for letter in myList:
+    myList.append(letter)
+    print(myList)
